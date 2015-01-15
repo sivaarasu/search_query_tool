9498cd1abad38f0447a0326ea3343b8eb7f01aab
siva arasu <siva.arasu@lazada.com>
2015-01-15 19:14:14 +0700

    first implementation

---
+++ bin/config.py
@@ -0,0 +1,12 @@
+from rocketads.utils.misc import getmypath
+import pandas as pd
+from rocketads.adwords.templates import ConvDict
+
+path = getmypath(__file__)
+
+OPTIONS = pd.read_csv(path+'/parameters.csv')
+
+
+
+
+
